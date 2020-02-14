5916ed2bb7812f4f0c73cbf571b34290e74e4ae9
Walid Mujahid وليد مجاهد <walid.mujahid.dev@gmail.com>
2020-02-14 16:29:16 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
-
-
 # name: discourse-foam-reputation
 # about: Validate that users have correct amount of reputation to signup/log in
 # version: 0.5.0
 # authors: 
 # url: https://github.com/walidmujahid/discourse-foam-reputation
-
-
 source "https://rubygems.org"
-
 
 gem 'http-accept', '1.7.0'
 gem 'http-cookie', '1.0.2'
@@ -17,7 +12,9 @@ gem 'mime-types', '1.16'
 gem 'netrc', '0.8'
 gem 'rest-client', '2.1.0'
 
+
 enabled_site_setting :foam_reputation_enabled
+
 
 after_initialize do
   require 'rest-client'
@@ -28,7 +25,12 @@ after_initialize do
     belongs_to :master_user, class_name: 'User'
     
     has_one :ethereum_address
+    
+    def activate
+    end
+
+    def deactivate
+    end
   end
-  
   
 end
